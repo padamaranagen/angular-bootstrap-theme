62345a24f4fc4fa3debece1319727a4a50764f82
padamaranagen <nagendramca2011@gmail.com>
2022-02-02 21:23:35 +0530

    Bootstrap and font-awesome libraries added with Header basic code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NpHeaderComponent } from './np-header/np-header.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, NpHeaderComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/np-header/np-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-np-header',
+  templateUrl: './np-header.component.html',
+  styleUrls: ['./np-header.component.css']
+})
+export class NpHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
