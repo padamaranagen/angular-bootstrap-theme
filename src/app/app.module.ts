43827e095353e9a1c431473b41b03fac4bfb8bf0
padamaranagen <nagendramca2011@gmail.com>
2022-02-02 21:26:58 +0530

    Footer component created with sample code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NpHeaderComponent } from './np-header/np-header.component';
+import { NpPageNotFoundComponent } from './np-page-not-found/np-page-not-found.component';
+import { NpFooterComponent } from './np-footer/np-footer.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, NpHeaderComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NpHeaderComponent,
+    NpPageNotFoundComponent,
+    NpFooterComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/np-footer/np-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-np-footer',
+  templateUrl: './np-footer.component.html',
+  styleUrls: ['./np-footer.component.css']
+})
+export class NpFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
