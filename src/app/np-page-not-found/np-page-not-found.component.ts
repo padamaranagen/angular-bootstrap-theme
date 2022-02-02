9b1229569e8266d88b3b158cefc047a7a42614a9
padamaranagen <nagendramca2011@gmail.com>
2022-02-02 19:34:28 +0530

    Basic components added

---
+++ src/app/np-page-not-found/np-page-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-np-page-not-found',
+  templateUrl: './np-page-not-found.component.html',
+  styleUrls: ['./np-page-not-found.component.css']
+})
+export class NpPageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
